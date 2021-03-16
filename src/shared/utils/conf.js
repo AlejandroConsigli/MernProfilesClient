f80f3d4d6863f922eca8f2034dd2e901a8ef5a16
Alejandro Consigli <alejandroconsigli@MacBook-Pro-de-Alejandro.local>
2021-03-15 22:10:26 -0300

    Change server url

--- src/shared/utils/conf.js
+++ src/shared/utils/conf.js
@@ -6,5 +6,5 @@ export const headers = {
 
 export const profileDefaultImage = "/assets/images/profile.jpg";
 
-axios.defaults.baseURL = process.env.NODE_ENV  !== "development" 
-    ? "http://localhost:5000" : "https://mernprofiles.herokuapp.com";
+axios.defaults.baseURL = process.env.NODE_ENV  === "development" 
+    ? "http://localhost:5000" : "https://main-mernpro-w7spw4c86a96vqot-gtw.qovery.io";
